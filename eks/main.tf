9c835642cd48f787d59f45b10d69c7f9c521e792
renan <renan.deud@pop-es.rnp.br>
2024-03-02 17:45:42 -0300

    adding

--- eks/main.tf
+++ eks/main.tf
@@ -80,12 +80,12 @@ provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
 
-  # exec {
-  #   api_version = "client.authentication.k8s.io/v1beta1"
-  #   command     = "aws"
-  #   # This requires the awscli to be installed locally where Terraform is executed
-  #   args = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
-  # }
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    # This requires the awscli to be installed locally where Terraform is executed
+    args = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
+  }
 }
 
 #aws eks --region $(terraform output -raw region) update-kubeconfig --name $(terraform output -raw cluster_name)
