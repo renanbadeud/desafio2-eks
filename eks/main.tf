ab9aeea2e0db3905783bfc5e4597f28bdce6d2a4
renan <renan.deud@pop-es.rnp.br>
2024-03-02 16:50:38 -0300

    adding

--- eks/main.tf
+++ eks/main.tf
@@ -77,9 +77,9 @@ module "eks" {
 }
 
 provider "kubernetes" {
-  host                   = data.aws_eks_cluster.cluster.endpoint
-  token                  = data.aws_eks_cluster_auth.cluster.token
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  token                  = module.aws_eks_cluster_auth.cluster.token
 }
 
 #aws eks --region $(terraform output -raw region) update-kubeconfig --name $(terraform output -raw cluster_name)
